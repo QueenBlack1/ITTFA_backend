4ceaa30ef7eff826ded16b67c9b3f2e77343635d
Agnes Tereka <tereka.agnes@icloud.com>
2026-06-23 05:59:18 +0200

    Update cloud_import.php

--- backend/cloud_import.php
+++ backend/cloud_import.php
@@ -1,4 +1,6 @@
 <?php
+header("Access-Control-Allow-Origin:*");
+header("Content-Type: application/json; charset=UTF-8");
 include "db_connection.php";
 
 $sql = file_get_contents("../winedine.sql");
